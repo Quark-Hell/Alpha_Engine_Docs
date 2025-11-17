e64cfc0fc1e933f99305e4743321a17b01fdc582
Quark <gennadiy.kaydalov@mail.ru>
2025-11-18 01:53:14 +0300

    changed bd create method

--- DocsGenerator/DocsGenerator/Program.cs
+++ DocsGenerator/DocsGenerator/Program.cs
@@ -23,9 +23,8 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate();
+    db.Database.EnsureCreated();
 }
-
 
 app.MapDefaultEndpoints();
 
